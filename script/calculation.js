ead808e118f8e102cd13397f77447ddbfc1d97ef
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2020-09-07 00:03:45 +0300

    Add keydown functionallity
    Add keydown functionallity and fix minor bugs

--- script/calculation.js
+++ script/calculation.js
@@ -13,40 +13,47 @@ const historyDiv = document.getElementById("history");
 const buttonsShowHide = document.getElementById("show/hide");
 const displayCurrent = document.getElementById("current");
 const displayPrevious = document.getElementById("previous");
+const bodyDiv = document.getElementById("body");
 let currSymbol = "";
 
 buttonsArr.forEach(element => element.addEventListener("click", function (event) {
-    let currSymbol = event.target.textContent;
+    currSymbol = event.target.textContent;
     let currNumber = displayCurrent.textContent;
     displayCurrent.textContent += calc.appendCurrent(currSymbol, currNumber);
 }));
 
-operandsArr.forEach(element => element.addEventListener("click", function(event) {
+operandsArr.forEach(element => element.addEventListener("click", function (event) {
     currSymbol = event.target.textContent;
     let currNumber = displayCurrent.textContent;
 
     if (displayCurrent.textContent.length > 0) {
         const result = calc.appendPrevious(currSymbol, currNumber);
-        
+
         if (result.length > 0) {
             displayPrevious.textContent = result;
             displayCurrent.textContent = "";
         }
+    } else {
+        let tempDisplay = displayPrevious.textContent;
+        displayPrevious.textContent = tempDisplay.substring(0, tempDisplay.length - 2) + " " + currSymbol;
     }
 }));
 
-
-calculation.addEventListener("click", function(event) {
+calculation.addEventListener("click", function (event) {
+    currSymbol = String(displayPrevious.textContent).substring(displayPrevious.textContent.length - 1);
     let currNumber = displayCurrent.textContent;
     let previousNumber = displayPrevious.textContent;
 
+    if (displayPrevious.textContent.includes("=")) {
+        return;
+    }
     if (currNumber.length > 0 && previousNumber.length > 0) {
         displayPrevious.textContent += " " + displayCurrent.textContent + " =";
         displayCurrent.textContent = calc.calculation(currSymbol, currNumber, previousNumber);
     }
 });
 
-deleteLastDigit.addEventListener("click", function(event) {
+deleteLastDigit.addEventListener("click", function (event) {
     let currNumber = displayCurrent.textContent;
 
     if (currNumber.length > 0) {
@@ -54,7 +61,7 @@ deleteLastDigit.addEventListener("click", function(event) {
     }
 });
 
-deleteAll.addEventListener("click", function() {
+deleteAll.addEventListener("click", function () {
     displayCurrent.textContent = "";
     displayPrevious.textContent = "";
     calc.clearAll;
@@ -64,7 +71,7 @@ deleteAll.addEventListener("click", function() {
     }
 });
 
-historyButton.addEventListener("click", function(event) {
+historyButton.addEventListener("click", function (event) {
     const historyArr = calc.history;
     let length = historyArr.length - 1;
 
@@ -76,7 +83,7 @@ historyButton.addEventListener("click", function(event) {
             historyDiv.appendChild(newDiv);
             length -= 5;
         }
-        
+
         historyDiv.classList.remove("hide");
         buttonsShowHide.classList.add("hide");
     } else {
@@ -84,3 +91,71 @@ historyButton.addEventListener("click", function(event) {
         buttonsShowHide.classList.remove("hide");
     }
 });
+
+bodyDiv.addEventListener("keydown", function (event) {
+    const symbolCodes = ['Numpad0', 'Numpad1', 'Numpad2', 'Numpad3', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9', 'NumpadDecimal',
+        'Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Comma', 'Period'];
+    const operandsCodes = ['NumpadAdd', 'NumpadSubstract', 'NumpadMultiply', 'NumpadDivide'];
+    const calculationStart = ['NumpadEnter', 'Enter', 'Space'];
+
+    if (symbolCodes.includes(event.code)) {
+        switch (event.code) {
+            case 'Comma':
+                currSymbol = ".";
+                break;
+            case 'Period':
+                currSymbol = ".";
+                break;
+            case 'NumpadDecimal':
+                currSymbol = ".";
+                break;
+            default:
+                let temp = String(event.code);
+                currSymbol = temp.substring(temp.length - 1);
+                break;
+        }
+        let currNumber = displayCurrent.textContent;
+        displayCurrent.textContent += calc.appendCurrent(currSymbol, currNumber);
+    } else if (operandsCodes.includes(event.code) && displayCurrent.textContent.length > 0) {
+        switch (event.code) {
+            case 'NumpadAdd':
+                currSymbol = '+';
+                break;
+            case 'NumpadSubstract':
+                currSymbol = '-';
+                break;
+            case 'NumpadMultiply':
+                currSymbol = '*';
+                break;
+            case 'NumpadDivide':
+                currSymbol = '/';
+                break;
+        }
+
+        let currNumber = displayCurrent.textContent;
+
+        if (displayCurrent.textContent.length > 0) {
+            const result = calc.appendPrevious(currSymbol, currNumber);
+
+            if (result.length > 0) {
+                displayPrevious.textContent = result;
+                displayCurrent.textContent = "";
+            }
+        } else {
+            let tempDisplay = displayPrevious.textContent;
+            displayPrevious.textContent = tempDisplay.substring(0, tempDisplay.length - 2) + " " + currSymbol;
+        }
+    } else if (calculationStart.includes(event.code)) {
+        currSymbol = String(displayPrevious.textContent).substring(displayPrevious.textContent.length - 1);
+        let currNumber = displayCurrent.textContent;
+        let previousNumber = displayPrevious.textContent;
+    
+        if (displayPrevious.textContent.includes("=")) {
+            return;
+        }
+        if (currNumber.length > 0 && previousNumber.length > 0) {
+            displayPrevious.textContent += " " + displayCurrent.textContent + " =";
+            displayCurrent.textContent = calc.calculation(currSymbol, currNumber, previousNumber);
+        }
+    }
+})
